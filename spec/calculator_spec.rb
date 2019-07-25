d77bf9b647f4d95bbfb70a2f9cb79f80bccf8b83
imperative-genre-4923 <dreama.lin@yale.edu>
2019-07-25 20:30:50 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,7 +9,7 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called second_number that is assigned to a number that isn't 0" do
-    second_number = 3.1
+    second_number = 3.2
 
     expect(second_number).to be_an(Integer).or be_a(Float)
     expect(second_number).not_to equal(0)
@@ -18,7 +18,7 @@ describe "./calculator.rb" do
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
     second_number = get_variable_from_file('./calculator.rb', "second_number")
-    sum = 4.1
+    sum = 4.2
 
     expect(sum).to eq(first_number+second_number)
   end
